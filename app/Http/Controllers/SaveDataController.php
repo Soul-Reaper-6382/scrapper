f2cb2099a879362d13af4a7842103f3ed131a5da
Soul-Reaper-6382 <hhshahofficial@gmail.com>
2025-04-17 02:30:00 +0500

    sdcvzzsaa

--- app/Http/Controllers/SaveDataController.php
+++ app/Http/Controllers/SaveDataController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\DataScrapper; // Adjust this to your actual model
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Session;
 
 class SaveDataController extends Controller
 {
