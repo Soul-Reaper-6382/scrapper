b5753a15494a1d5a58c62dc5c1e5dcc609a97c20
Soul-Reaper-6382 <hhshahofficial@gmail.com>
2025-06-02 03:02:35 +0500

    sss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,7 +84,9 @@
             </div>
         </div>
     </div>
-
+<p>Access Token: {{ session('access_token') }}</p>
+<p>User ID: {{ session('user_id') }}</p>
+<p>Store ID: {{ session('store_id') }}</p>
 <script type="module">
 window.storeid = @json(session('store_id')); // This will embed the session value in JavaScript
 
